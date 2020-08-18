c3e16dd3708ede1db08587b30c098e3282c45ec4
Kanayana1205 <kana.3031@gmail.com>
2020-08-18 15:50:06 +0900

    add new file

--- ABCanalysis.py
+++ ABCanalysis.py
@@ -1,6 +1,8 @@
 # モジュールのインポート
 import os, tkinter, tkinter.filedialog, tkinter.messagebox
 import openpyxl as px
+import pandas as pd
+import csv
 
 # ファイル選択ダイアログの表示
 root = tkinter.Tk()
@@ -16,6 +18,13 @@ file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
 # 処理ファイル名の出力
 #tkinter.messagebox.showinfo('○×プログラム',file)
 
-wb = px.Workbook()
-wb.save("ABCanalysis.xlsx")
+#データをdfに読み込み。pandasをpdとして利用。
+df = pd.read_csv(file,encoding='cp932',names=('JANcode', '商品名', '分析値'))
+
+print(df)
+#wb = px.Workbook()
+#ws = wb.active
+df.to_excel('ABCanalysis.xlsx',sheet_name='ABCanalysis',index=False)
+
+#wb.save("ABCanalysis.xlsx")
 
