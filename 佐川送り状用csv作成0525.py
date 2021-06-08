efc5cb8bc4643b44a772eae711832dfcad6fc16d
TWHN26 <nishizawa@towasan.co.jp>
2021-06-08 11:18:59 +0900

    modify

--- 佐川送り状用csv作成0525.py
+++ 佐川送り状用csv作成0525.py
@@ -23,8 +23,8 @@ filename = '佐川送り状データ_' + now.strftime('%Y%m%d') + '.csv'
 
 
 #指定フォルダ内のxlsxファイルを全て取得
-files = glob.glob('X:/通箱/★システム管理部/西澤/佐川送り状/*.xls*')
-#files = glob.glob("C:/Users/TWHN26/Desktop/CSV転記テスト/*.xls*")
+#files = glob.glob('X:/通箱/★システム管理部/西澤/佐川送り状/*.xls*')
+files = glob.glob("C:/Users/TWHN26/Desktop/CSV転記テスト/*.xls*")
 
 #---------------------------------------------------------------------------------------------------------------------
 #まず、入力エラー（文字制限）を全ファイルチェックし、該当ファイルをメッセージボックスに表示させる。なければ続行-----------------
@@ -55,9 +55,9 @@ if not len(error_file) == 0:
 for file in files:
     wb = px.load_workbook(file, data_only=True)
     
-    #すべてのワークシートに対して作業する。ただし、F5に「着日」の文字があるかどうかで条件分岐
+    #すべてのワークシートに対して作業する。ただし、F5に「着日」の文字があるか、C5に「〒郵便番号」、その他かどうかで条件分岐
     for ws in wb.worksheets:
-        
+        #通常サンプル、POP依頼書の場合-------------------------------------------------------------------
         if  ws.cell(row = 5,column = 6).value == "着日" :
             ex_postal_code = ws["C7"].value
             #郵便番号に全角が入っていた場合（空白でなければ）半角にする
@@ -102,6 +102,41 @@ for file in files:
                 writer = csv.writer(f)
                 writer.writerow(data)
 
-        #F5に「着日」の文字が無いシートは無視
+        #　C5に「〒郵便番号」（複数送り先パターン）の場合--------------------------------------------------------------------------
+        elif ws.cell(row = 5,column = 3).value == "〒郵便番号" :
+            #最終行を取得
+            maxRow = ws.max_row + 1
+
+            for i in range(7,maxRow):
+
+                ex_postal_code = ws.cell(row = i ,column = 3).value
+                #郵便番号に全角が入っていた場合（空白でなければ）半角にする
+                if not ex_postal_code is None:
+                    ex_postal_code = mojimoji.zen_to_han(ex_postal_code)
+                postal_code = ex_postal_code
+                address_1 = ws.cell(row = i ,column = 4).value
+                address_2 = ws.cell(row = i ,column = 5).value
+                address_3 = ws.cell(row = i ,column = 6).value
+            
+                to_name_1 = ws.cell(row = i ,column = 7).value
+                to_name_2 = ws.cell(row = i ,column = 8).value
+                tel = ws.cell(row = i ,column = 2).value
+                title = ws.cell(row = i ,column = 9).value
+
+                day = ""
+                morning = ""
+
+                #csvへ入力する為のリスト
+                data = ['',tel,postal_code,address_1,address_2,address_3,\
+                    to_name_1,to_name_2,'',348244210016,'','','','','','',\
+                    '','','001',title,'','','','',1,'000','001',day,morning,'',0,'','','','','005','','','','','',1]
+
+
+                with open(filename, 'a',newline="") as f:
+                    writer = csv.writer(f)
+                    writer.writerow(data)
+
+
+        #F5に「着日」の文字が無い、C5に「〒郵便番号」がないシートは無視
         else:
             pass
