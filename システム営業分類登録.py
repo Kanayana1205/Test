b647e3d49fd8aca4f9095be590d501a1c147e73d
Kanayana1205 <kana.3031@gmail.com>
2021-06-23 10:00:09 +0900

    new file

---
+++ システム営業分類登録.py
@@ -0,0 +1,74 @@
+import pyautogui as pg
+import os, tkinter, tkinter.filedialog, tkinter.messagebox
+import pandas as pd
+import csv
+import time
+
+
+
+root = tkinter.Tk()
+root.withdraw()
+
+# CSVファイル指定   
+fTyp = [("","*.csv")]
+# ファイル選択ダイアログの表示
+iDir = os.path.abspath(os.path.dirname(__file__))
+tkinter.messagebox.showinfo("営業分類自動登録","登録用ファイルを選択してください！")
+file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+#CSVファイルの読み込み（リスト型）
+data = pd.read_csv(file).values.tolist()
+
+#システム登録画面を見えるようにしておく（分類のタブに特に注意）
+
+#商品コード欄をクリック
+x, y = pg.locateCenterOnScreen("X:\\通箱\\★東京営業所\\西澤\\商品登録.PNG")
+pg.click(x, y)
+
+#分類（営業）タブの座標を変数へ入れる
+a, b = pg.locateCenterOnScreen("X:\\通箱\\★東京営業所\\西澤\\分類.PNG")
+
+
+for index in range(len(data)):
+    #10アイテム登録ごとに、スクリーンショットをとって保存。（予期せぬ場合の記録）
+    if  index == 0 :
+        pass
+    elif index % 10 == 0 :
+        pg.hotkey("winleft","printscreen")
+        time.sleep(2)
+    else :
+        pass
+
+    
+    #商品コードを入力、エンターを押す
+    pg.typewrite(data[index][0])
+    time.sleep(1)
+    pg.press("enter")
+    #反応が遅いので、15秒待機
+    time.sleep(23)
+    #分類（営業）タブをクリック
+    pg.click(a, b)
+    time.sleep(3)
+    #営業部分類１をアクティブにする
+    pg.press("tab")
+    time.sleep(1)
+    #営業部分類コードを入力、エンターを押す
+    pg.typewrite(str(data[index][1]))
+    time.sleep(1)
+    pg.press("enter")
+    time.sleep(1)
+    #ｆ1キーで登録
+    pg.press("f1")
+    #反応が遅いので、3秒待機
+    time.sleep(3)
+    #「保存しますか？」で「はい」の為のエンターキー
+    pg.press("enter")
+    #反応が遅いので、5秒待機
+    time.sleep(5)
+
+
+
+
+
+
+
+
